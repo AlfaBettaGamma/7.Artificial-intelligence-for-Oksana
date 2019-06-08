5d08624353caa8f20e5a750c1ee859f54b1fb84c
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-06-09 01:04:55 +0300

    update

--- test.py
+++ test.py
@@ -3,23 +3,18 @@ def SumOfThe( N,  data):
   rez = 0
   for i in range(N):
     s = s + data[i]
-  print(s)
   for j in range(N):
     if s < 0 and data[j] > 0:
-      print('Входяшие 1 - ',data[j])
       if s + data[j] == data[j]:
         rez = data[j]
     elif s < 0 and data[j] < 0:
-      print('Входяшие 2 - ',data[j])
       if s - data[j] == data[j]:
         rez = data[j]
     elif s > 0 and data[j] > 0:
-      print('Входяшие 3 - ',data[j])
       if s - data[j] == data[j]:
         rez = data[j]
     elif s > 0 and data[j] < 0:
-      print('Входяшие 4 - ',data[j])
       if s + data[j] == data[j]:
         rez = data[j]
         
-  print(rez)
+  return rez
